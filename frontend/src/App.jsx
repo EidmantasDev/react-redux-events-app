7e07fd3e78704daeae2f37ba85f52946a4ee95bc
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-20 23:03:19 +0300

    Added new pages and init routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home/Home';
+import EventsPage from './pages/Events/Events';
+import EventDetailPage from './pages/EventDetail/EventDetail';
+import NewEventPage from './pages/NewEvent/NewEvent';
+import EditEventPage from './pages/EditEvent/EditEvent';
+import RootLayout from './pages/Root/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: 'events', element: <EventsPage /> },
+      { path: 'events/:id', element: <EventDetailPage /> },
+      { path: 'events/new', element: <NewEventPage /> },
+      { path: 'events/:id/edit', element: <EditEventPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- frontend/src/components/MainNavigation/MainNavigation.jsx
+++ frontend/src/components/MainNavigation/MainNavigation.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { header, list } from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -6,10 +7,10 @@ function MainNavigation() {
       <nav>
         <ul className={list}>
           <li>
-            <a>Home</a>
+            <NavLink to='/'>Home</NavLink>
           </li>
           <li>
-            <a>Events</a>
+            <Link to='/events'>Events</Link>
           </li>
         </ul>
       </nav>
