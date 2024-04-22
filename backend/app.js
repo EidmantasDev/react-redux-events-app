59206675f176262774d0a62d15e1d5627c10dfc0
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-22 20:41:18 +0300

    Added new Authentication Page Added new AuthForm component with styling Added util/auth file to backend Added routes/auth file to backend Added data/util file to backend Added data/user file to backend Added .env-example Installed bcryptjs jsonwebtoken packages Updated app in the backend to use auth routes

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
+
+app.use(authRoutes);
 
 app.use('/events', eventRoutes);
 
