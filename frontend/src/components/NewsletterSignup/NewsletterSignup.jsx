005c30b1ff9d66e0508b9a0d36970975ade2ebcc
EidmantasDev <eidmantas.slenys@gmail.com>
2024-04-22 18:05:08 +0300

    Updated NewsletterSignup component to use useFetcher from react-router-dom Updated Events page to use defer for loadEvents actions and display Loading message while waiting for events to be loaded

--- frontend/src/components/NewsletterSignup/NewsletterSignup.jsx
+++ frontend/src/components/NewsletterSignup/NewsletterSignup.jsx
@@ -1,15 +1,25 @@
+import { useFetcher } from 'react-router-dom';
 import { newsletter } from './NewsletterSignup.module.css';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
+  const { Form, data, state } = useFetcher();
+
+  useEffect(() => {
+    if (state === 'idle' && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
   return (
-    <form method='post' className={newsletter}>
+    <Form method='post' action='/newsletter' className={newsletter}>
       <input
         type='email'
         placeholder='Sign up for newsletter...'
         aria-label='Sign up for newsletter'
       />
       <button>Sign up</button>
-    </form>
+    </Form>
   );
 }
 
--- frontend/src/pages/Events/Events.jsx
+++ frontend/src/pages/Events/Events.jsx
@@ -1,24 +1,34 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 import EventsList from '../../components/EventsList/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
   const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
     return json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const { events } = await response.json();
+    return events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
